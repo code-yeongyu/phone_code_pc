f060fc2798cf9f9767e0a9dcdf27687c7be46c58
kim-yeon-gyu-exlock <public.kim.yeon.gyu@gmail.com>
2018-06-21 23:46:44 +0900

    added : functions that works for locating blocks properly

--- app/src/main/java/exlock/phonecode_pc/EditActivity.java
+++ app/src/main/java/exlock/phonecode_pc/EditActivity.java
@@ -5,7 +5,6 @@ import android.os.Environment;
 import android.os.Bundle;
 import android.graphics.Color;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +14,8 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import exlock.phonecode_pc.Tools.ManageCode;
 
@@ -34,7 +35,8 @@ import exlock.phonecode_pc.Tools.ManageCode;
 * add string
 * add block below/above
 *
-* add EditText's value next to function inside bracket
+* EditText should be located between two related brackets,
+* so split between "(" and closest ")" and locate EditText which has the content inside of them there.
 *
 * if cursor is inside of EditText, add function's value there
 *
@@ -65,6 +67,7 @@ public class EditActivity extends AppCompatActivity {
         String[] lines = content.split("\n");
 
         for(;itemsInDisplayCodeLayout<lines.length;itemsInDisplayCodeLayout++){
+            final String functionString = lines[itemsInDisplayCodeLayout];
             LinearLayout tempBlockLayout = new LinearLayout(getApplication());
             /*Todo:
             * shows menu if tempBlockLayout was clicked for a long time :
@@ -74,7 +77,7 @@ public class EditActivity extends AppCompatActivity {
             */
             tempBlockLayout.setOrientation(LinearLayout.HORIZONTAL);
             TextView tv = new TextView(getApplication());
-            final String functionString = lines[itemsInDisplayCodeLayout];
+
             tv.setText(functionString);
             EditText et = new EditText(getApplication());
             /*Todo:
@@ -85,6 +88,57 @@ public class EditActivity extends AppCompatActivity {
             displayCodeLayout.addView(tempBlockLayout);
         }
     }
+    private ArrayList<Integer> findStringPositions(String source, String target){
+        int position = -300;
+        int length = source.length();
+        final ArrayList<Integer> positions = new ArrayList<>();
+
+        for(;;){
+            position = source.indexOf(target);
+            if(position==-1){
+                break;
+            }
+            positions.add(length-source.length()+position);
+            source = source.substring(position+1, source.length());
+        }
+        return positions;
+    }
+    private ArrayList<Integer> getBracketPairs(ArrayList<Integer> left, ArrayList<Integer> right){
+        ArrayList<Integer> pairs = new ArrayList<>();
+        left.addAll(right);
+
+        if(left.size()%2==0) {//if it's able to get pairs
+            Log.d("getPairs", "passed if");
+            Collections.sort(left, new Ascending());
+            Log.d("getPairs", "worked fine, results: " + left);
+            int leftSize = left.size();
+            while(leftSize!=0){
+                int temp = (leftSize / 2)-1;
+                pairs.add(left.get(temp));
+                pairs.add(left.get(temp+1));
+                left.remove(temp);
+                left.remove(temp);
+                leftSize-=2;
+            }
+            Log.d("getPairs", "worked fine, results: " + pairs);
+            return pairs;
+        }
+        return null;
+    }
+    private void block(String function){//return type is going to change
+        ArrayList<Integer> leftBracketsPositions = findStringPositions(function, "(");
+        ArrayList<Integer> rightBracketsPositions = findStringPositions(function, ")");
+        ArrayList<Integer> a = getBracketPairs(rightBracketsPositions, leftBracketsPositions);
+
+        TextView functionName = new TextView(getApplication());
+        EditText bracket = new EditText(getApplication());
+        //split string 'function' with a's values.
+        //and display a textView with it.
+        //and between a's odd and even, locate 'bracket'
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -132,8 +186,7 @@ public class EditActivity extends AppCompatActivity {
             final String strCategory = categories.get(i).toString();//A string variable that contains a category's name
             final Button category = new Button(getApplication());
             final int forAssign = i+5000;
-            category.setId(forAssign);
-            View.OnClickListener categoryOnClickListener = new View.OnClickListener() {
+            View.OnClickListener categoryOnClickListner = new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     controlMenu(closeButton, categoriesLayout,true);
@@ -158,7 +211,6 @@ public class EditActivity extends AppCompatActivity {
                                 */
                                     content = content + "\n" + lp.getFunctionValue(strCategory, strFunctions);
                                     mc.setContent(content);
-                                    Log.d("well", lp.getFunctionValue(strCategory, strFunctions));
                                     addItemsInDisplayCodeLayout();
                                 }
                             });
@@ -168,13 +220,13 @@ public class EditActivity extends AppCompatActivity {
                     }
                 }
             };
+            category.setId(forAssign);
             category.setText(strCategory);
-            category.setOnClickListener(categoryOnClickListener);//on Button Clicked, load functions
+            category.setOnClickListener(categoryOnClickListner);//on Button Clicked, load functions
             tempCategoriesLayout.addView(category);
         }
         categoriesLayout.addView(tempCategoriesLayout);
     }
-
     public void controlMenu(Button closeButton, LinearLayout categoriesLayout, Boolean isRequestClose){
         if(isRequestClose) {
             categoriesLayout.setVisibility(View.GONE);
@@ -189,4 +241,13 @@ public class EditActivity extends AppCompatActivity {
         * change scrollBlocksView's size to 50 if isRequestClose, or 200
         */
     }
+}
+
+class Ascending implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o1.compareTo(o2);
+    }
+
 }
