2cb5f7a687ff2586273e6af6781072bd2295fcd1
Yeon-Gyu-Kim <public.kim.yeon.gyu@gmail.com>
2018-08-07 11:35:31 +0900

    Update: LanguageProfileMember.java

--- app/src/main/java/exlock/phonecode_pc/Tools/LanguageProfileMember.java
+++ app/src/main/java/exlock/phonecode_pc/Tools/LanguageProfileMember.java
@@ -3,6 +3,6 @@ package exlock.phonecode_pc.Tools;
 import java.util.ArrayList;
 
 public class LanguageProfileMember {
-    String name, version, wayToCreateVar;
-    ArrayList<String> categories, symbols, reserved, reservedObject;
+    public String json, name, version, wayToCreateVar;
+    public ArrayList<String> categories, symbols, reserved, reservedObject;
 }
