2107abf6b8686bbb0f3e9a4088cfbe58391205a7
Yeon-Gyu-Kim <public.kim.yeon.gyu@gmail.com>
2018-08-07 11:37:05 +0900

    Update: SettingActivity.java
    saving the path of the file feature

--- app/src/main/java/exlock/phonecode_pc/SettingActivity.java
+++ app/src/main/java/exlock/phonecode_pc/SettingActivity.java
@@ -1,11 +1,19 @@
 package exlock.phonecode_pc;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import android.net.Uri;
+
+import exlock.phonecode_pc.Tools.JsonManager;
+import exlock.phonecode_pc.Tools.LanguageProfile;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -28,8 +36,35 @@ public class SettingActivity extends AppCompatActivity {
                 i.setAction(Intent.ACTION_OPEN_DOCUMENT);
                 i.setType("application/*");
                 i.addCategory(Intent.CATEGORY_OPENABLE);
-                    startActivityForResult(Intent.createChooser(i,"Select the language profiies"), 0);
+                    startActivityForResult(Intent.createChooser(i,"Select the language profiies"), 43);
             }
         });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent resultData) {
+
+        if (requestCode == 43 && resultCode == Activity.RESULT_OK) {
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                String selectedFile = uri.toString();
+                String jsonExtension = selectedFile.substring(selectedFile.length() - 4, selectedFile.length());
+                if (!jsonExtension.equals("json")) {
+                    Toast.makeText(SettingActivity.this, getString(R.string.toast_not_json), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String[] path = uri.getPath().split(":");
+                String json = JsonManager.getJsonFromPath(Environment.getExternalStorageDirectory() + path[1]);
+                if (LanguageProfile.getProfileMembers(json) == null) {
+                    Toast.makeText(SettingActivity.this, getString(R.string.toast_wrong_json), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                SharedPreferences sp = getSharedPreferences("json", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("profileJson", json);
+                editor.apply();
+            }
+        }
     }
 }
