a1119575b85b94650d71edac42eb27d6f8ac9492
Yeon-Gyu-Kim <public.kim.yeon.gyu@gmail.com>
2018-08-08 23:27:17 +0900

    Add: merging language profile feature

--- app/src/main/java/exlock/phonecode_pc/SettingActivity.java
+++ app/src/main/java/exlock/phonecode_pc/SettingActivity.java
@@ -16,11 +16,17 @@ import android.widget.Toast;
 
 import android.net.Uri;
 
-import java.lang.reflect.Array;
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import exlock.phonecode_pc.Tools.JsonManager;
+import exlock.phonecode_pc.Tools.LanguageProfile;
 import exlock.phonecode_pc.Tools.LanguageProfileJsonReader;
 import exlock.phonecode_pc.Tools.LanguageProfileMember;
 
@@ -30,8 +36,10 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
         final TextView indentSetting = findViewById(R.id.settingIndentTextView);
         TextView profileSetting = findViewById(R.id.settingLanguageProfileTextView);
+        TextView addProfile = findViewById(R.id.settingAddLanguageProfileTextView);
 
         final String jsonString = getSharedPreferences("json", MODE_PRIVATE)
                         .getString("profileJson", "");
@@ -52,35 +60,113 @@ public class SettingActivity extends AppCompatActivity {
                     startActivityForResult(Intent.createChooser(i,"Select the language profiies"), 43);
             }
         });
+        addProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_OPEN_DOCUMENT);
+                i.setType("application/*");
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(Intent.createChooser(i,"Select the language profiies"), 44);
+            }
+        });
     }
     @Override
-    public void onActivityResult(int requestCode, int resultCode,
-                                 Intent resultData) {
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
 
-        if (requestCode == 43 && resultCode == Activity.RESULT_OK) {
-            Uri uri = null;
-            if (resultData != null) {
-                uri = resultData.getData();
-                String selectedFile = uri.toString();
-                String jsonExtension = selectedFile.substring(selectedFile.length() - 4, selectedFile.length());
-                if (!jsonExtension.equals("json")) {
-                    Toast.makeText(SettingActivity.this, getString(R.string.toast_not_json), Toast.LENGTH_SHORT).show();
-                    return;
+        if (resultCode == Activity.RESULT_OK) {//if set the language profiles
+            if(requestCode == 43) {
+                Uri uri = null;
+                if (resultData != null) {
+                    uri = resultData.getData();
+                    String selectedFile = uri.toString();
+                    String jsonExtension = selectedFile.substring(selectedFile.length() - 4, selectedFile.length());
+                    if (!jsonExtension.equals("json")) {
+                        Toast.makeText(SettingActivity.this, getString(R.string.toast_not_json), Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    String[] path = uri.getPath().split(":");
+                    String absolutePath = Environment.getExternalStorageDirectory() + "/" + path[1];
+                    String json = JsonManager.getJsonFromPath(absolutePath);
+
+                    if (LanguageProfileJsonReader.getProfileMembers(json) == null) {
+                        Toast.makeText(SettingActivity.this, getString(R.string.toast_wrong_json), Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    SharedPreferences profileJson = getSharedPreferences("json", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = profileJson.edit();
+                    editor.putString("profileJson", json);
+                    editor.apply();
+
+                    setLanguageProfileDiretory(absolutePath);
                 }
-                String[] path = uri.getPath().split(":");
-                String json = JsonManager.getJsonFromPath(Environment.getExternalStorageDirectory() + path[1]);
-                if (LanguageProfile.getProfileMembers(json) == null) {
-                    Toast.makeText(SettingActivity.this, getString(R.string.toast_wrong_json), Toast.LENGTH_SHORT).show();
-                    return;
+
+            }else if(requestCode == 44){
+                Uri uri = null;
+                if (resultData != null) {
+                    uri = resultData.getData();
+                    String selectedFile = uri.toString();
+                    String jsonExtension = selectedFile.substring(selectedFile.length() - 4, selectedFile.length());
+
+                    if (!jsonExtension.equals("json")) {
+                        Toast.makeText(SettingActivity.this, getString(R.string.toast_not_json), Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    String[] path = uri.getPath().split(":");
+                    String absolutePath = Environment.getExternalStorageDirectory() + "/" + path[1];
+                    String newJson = JsonManager.getJsonFromPath(absolutePath);
+
+                    if (LanguageProfileJsonReader.getProfileMembers(newJson) == null) {
+                        Toast.makeText(SettingActivity.this, getString(R.string.toast_wrong_json), Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    SharedPreferences jsonSP = getSharedPreferences("json", MODE_PRIVATE);
+
+                    LanguageProfileMember oldLPM = LanguageProfileJsonReader.getProfileMembers(
+                            jsonSP.getString("profileJson", "")
+                    );
+                    LanguageProfileMember currLPM = LanguageProfileJsonReader.getProfileMembers(newJson);
+
+                    ArrayList<String> currCategories;
+
+                    if(oldLPM!=null&&currLPM!=null){
+                        currCategories = currLPM.categories;
+
+                        LanguageProfile oldLangProfile = new Gson().fromJson(
+                                jsonSP.getString("profileJson", ""),
+                                LanguageProfile.class
+                        );
+
+                        LanguageProfileJsonReader currLPJR = new LanguageProfileJsonReader(currLPM);
+
+                        ArrayList<String> temp = new ArrayList<>();
+
+                        if(currCategories.size() > 0) {
+                            temp.add(JsonManager.addJsonKeyToArray(
+                                    oldLangProfile.getFunctions().toString(),
+                                    currCategories.get(0),
+                                    currLPJR.getFunctions(currCategories.get(0))).toString());
+                            for (int i = 1; i < currCategories.size(); i++) {
+                                String targetString = currCategories.get(i);
+
+                                temp.add(JsonManager.addJsonKeyToArray(
+                                        temp.get(i - 1),
+                                        targetString,
+                                        currLPJR.getFunctions(targetString)
+                                ).toString());
+                            }
+                            SharedPreferences.Editor editor = jsonSP.edit();
+                            editor.putString("profileJson", temp.get(temp.size()-1));
+                            editor.apply();
+                        }
+                    }else{
+                        //error
+                    }
                 }
-                SharedPreferences sp = getSharedPreferences("json", MODE_PRIVATE);
-                SharedPreferences.Editor editor = sp.edit();
-                editor.putString("profileJson", json);
-                editor.apply();
             }
         }
     }
-    private Dialog indentSetDialog(final String jsonString){
+    private Dialog indentSetDialog(final String jsonString) {
         ArrayList<String> temp = new ArrayList<>();
         final String twoSpace = getString(R.string.setting_indent_two_space);
         final String fourSpace = getString(R.string.setting_indent_four_space);
@@ -102,15 +188,15 @@ public class SettingActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int which) {
                         String selected = list.get(which);
                         String result = "";
-                        if(selected.equals(twoSpace)){
+                        if (selected.equals(twoSpace)) {
                             result = "  ";
-                        }else if(selected.equals(fourSpace)){
+                        } else if (selected.equals(fourSpace)) {
                             result = "    ";
-                        }else if(selected.equals(eightSpace)){
+                        } else if (selected.equals(eightSpace)) {
                             result = "        ";
-                        }else if(selected.equals(oneTab)){
+                        } else if (selected.equals(oneTab)) {
                             result = "\t";
-                        }else if(selected.equals(twoTab)){
+                        } else if (selected.equals(twoTab)) {
                             result = "\t\t";
                         }
                         String modifiedJson = JsonManager.modifyJsonByKey(jsonString, "lang_informs", "indent", result);
@@ -122,5 +208,35 @@ public class SettingActivity extends AppCompatActivity {
                 })
                 .create();
         return dialog;
+    }
+    private void setLanguageProfileDiretory(String absolutePath){
+        ArrayList<String> pathsArray = new ArrayList<>();
+        pathsArray.add(absolutePath);
+        Set<String> set = new LinkedHashSet<>(pathsArray);
+        pathsArray.clear();
+        pathsArray.addAll(set);
+
+        SharedPreferences absolutePaths = getSharedPreferences("language_profile_paths", MODE_PRIVATE);
+        SharedPreferences.Editor editor = absolutePaths.edit();
+        editor.putString("lpp", new Gson().toJson(pathsArray));
+        editor.apply();
+    }
+    private void addLanguageProfileDiretory(String absolutePath){
+        ArrayList<String> pathsArray = new ArrayList<>();
+        SharedPreferences absolutePaths = getSharedPreferences("json", MODE_PRIVATE);
+
+        String prev = absolutePaths.getString("language_profile_paths", "");
+        if(!prev.equals("")) {
+            pathsArray.add(prev);
+        }
+
+        pathsArray.add(absolutePath);
+        Set<String> set = new LinkedHashSet<>(pathsArray);
+        pathsArray.clear();
+        pathsArray.addAll(set);
+
+        SharedPreferences.Editor editor = absolutePaths.edit();
+        editor.putString("language_profile_paths", new Gson().toJson(pathsArray));
+        editor.apply();
     }
 }
