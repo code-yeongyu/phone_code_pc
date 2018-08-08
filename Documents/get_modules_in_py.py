964e2da5228ce6f0cb5c8b0101ebf15b0371515d
Yeon-Gyu-Kim <public.kim.yeon.gyu@gmail.com>
2018-08-08 23:25:33 +0900

    Update: get_moduels_in_py.py
    Add some exceptions and cases for errors

--- Documents/get_modules_in_py.py
+++ Documents/get_modules_in_py.py
@@ -2,12 +2,18 @@ import sys
 
 
 def main():
-    funcs = str(dir(__import__(sys.argv[1])))
-    funcs = funcs.replace("\'", '\"').replace(',', ',\n')
-    funcs = '{"'+sys.argv[1]+'": '+funcs+"}"
-    file = open("function_lists"+sys.argv[1]+".json", 'w+')
-    file.write(funcs)
-    file.close()
-
+    if len(sys.argv) > 1 :
+        try :
+            funcs = str(dir(__import__(sys.argv[1])))
+        except ModuleNotFoundError:
+            print("You don't have that module.")
+            return;
+        funcs = funcs.replace("\'", '\"').replace(',', ',\n')
+        funcs = '{"'+sys.argv[1]+'": '+funcs+"}"
+        file = open("function_lists_"+sys.argv[1]+".json", 'w+')
+        file.write(funcs)
+        file.close()
+    else :
+        print("Type the module that you want to get the functions list by an argument.")
 
 main()
