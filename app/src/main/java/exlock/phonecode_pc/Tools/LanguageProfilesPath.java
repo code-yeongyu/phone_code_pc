f64530f1c6c8523d16a7f53c668373d4bb84b7be
Yeon-Gyu-Kim <public.kim.yeon.gyu@gmail.com>
2018-08-09 16:44:36 +0900

    Create: LanguageProfilesPath.java
    for managing json-typed array with java's object

---
+++ app/src/main/java/exlock/phonecode_pc/Tools/LanguageProfilesPath.java
@@ -0,0 +1,11 @@
+package exlock.phonecode_pc.Tools;
+
+import java.util.ArrayList;
+
+public class LanguageProfilesPath {
+    private ArrayList<String> paths;
+
+    public ArrayList<String> getPaths() {
+        return paths;
+    }
+}
