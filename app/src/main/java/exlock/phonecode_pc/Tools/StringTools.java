82fbecf580a02347a47c0f3683a5238851003169
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-09-29 01:50:42 +0900

    Revert "Remove: StringTools.java"
    This reverts commit 7451bbdc2ed4c7452894623638ba5fa5969793b2.

---
+++ app/src/main/java/exlock/phonecode_pc/Tools/StringTools.java
@@ -0,0 +1,28 @@
+package exlock.phonecode_pc.Tools;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class StringTools {
+    public static ArrayList<Integer> findStringPositions(String source, String target){
+        int position;
+        int length = source.length();
+        final ArrayList<Integer> positions = new ArrayList<>();
+
+        for(;;){
+            position = source.indexOf(target);
+            if(position==-1){
+                break;
+            }
+            positions.add(length-source.length()+position);
+            source = source.substring(position+1, source.length());
+        }
+        return positions;
+    }
+}
+class Descending implements Comparator<Integer> {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o2.compareTo(o1);
+    }
+}
